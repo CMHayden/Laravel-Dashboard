3fe7b3b57c50e449ed0ae4dbfc2de46f99bfd98f
CMHayden <haydencallum4@gmail.com>
2019-07-14 15:41:51 +0200

    create mounted property

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -28,10 +28,17 @@
 </template>
 <script>
 import { Cartesian, Area } from 'laue'
+import axios from 'axios'
 export default {
     components: {
         LaCartesian: Cartesian,
         LaArea: Area
+    },
+    mounted() {
+        axios.get('/api/dashboard')
+            .then((res) => {
+                this.$set(this.$data, 'cards', res.data.cards)
+            })
     },
     data() {
         return {
